5b9c070d5ec49837bf5c8f56526144149a362144
x3xmermaid <32973695+x3xmermaid@users.noreply.github.com>
2019-04-07 20:40:29 +0700

    Create Chatlist.java

---
+++ app/src/main/java/com/example/b34uty_m3rm41d/andro_project2/Model/Chatlist.java
@@ -0,0 +1,22 @@
+package com.example.b34uty_m3rm41d.andro_project2.Model;
+
+public class Chatlist {
+    public String id;
+
+    public Chatlist(String id) {
+        this.id = id;
+    }
+
+    public Chatlist(){
+
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
