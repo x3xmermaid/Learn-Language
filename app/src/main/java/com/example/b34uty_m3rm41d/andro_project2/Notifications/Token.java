a0b946681e078dc716fe001528ee1d79c25a5252
x3xmermaid <32973695+x3xmermaid@users.noreply.github.com>
2019-04-07 20:41:00 +0700

    Create Token.java

---
+++ app/src/main/java/com/example/b34uty_m3rm41d/andro_project2/Notifications/Token.java
@@ -0,0 +1,21 @@
+package com.example.b34uty_m3rm41d.andro_project2.Notifications;
+
+public class Token {
+    private String token;
+
+    public Token(String token) {
+        this.token = token;
+    }
+
+    public Token(){
+
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
